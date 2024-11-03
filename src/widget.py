36e4f1e306a37e2ae94f96ad8013dc22fa67023b
mikhail lemdyanov <lacoustique@yandex.ru>
2024-11-03 19:37:34 +0500

    get_date

--- src/widget.py
+++ src/widget.py
@@ -8,3 +8,8 @@ def mask_account_card(user_info: str) -> str:
         card_number = get_mask_card_number(user_info[-16:])
         card_mask = user_info.replace(user_info[-16:], card_number)
         return card_mask
+
+
+def get_date(date: str) -> str:
+    """Функция, которая возвращает дату в формате ДД.ММ.ГГГГ"""
+    return (f'{date[8:10]}.{date[5:7]}.{date[0:4]}')
