79c237442b3eb34d55419f138a5c3d1b51283ce6
mikhail lemdyanov <lacoustique@yandex.ru>
2024-11-03 19:29:52 +0500

    mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -2,4 +2,9 @@ from masks import get_mask_card_number, get_mask_account
 
 def mask_account_card(user_info: str) -> str:
     """Функция, маскирующая информацию как о картах, так и о счетах"""
-
+    if 'Счет' in user_info:
+        return ('Счет ' + get_mask_account(user_info))
+    else:
+        card_number = get_mask_card_number(user_info[-16:])
+        card_mask = user_info.replace(user_info[-16:], card_number)
+        return card_mask
