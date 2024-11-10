c5271b4e67360c0b0ec64c90404d9a35a83cee19
mikhail lemdyanov <lacoustique@yandex.ru>
2024-11-10 20:34:50 +0500

    fixes by flake8, black, mypy and isort

--- src/processing.py
+++ src/processing.py
@@ -13,5 +13,4 @@ def sort_by_date(list_of_dictionaries: list[dict[str, Any]], reverse_list: bool 
     """Функция, которая возвращает список словарей отсортированный по дате"""
 
     sorted_list_of_dictionaries = sorted(list_of_dictionaries, key=lambda x: x["date"], reverse=reverse_list)
-
     return sorted_list_of_dictionaries
