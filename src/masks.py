4439cbdeb9d2d7ef073e84b7a22dc082d8dfe8b5
mikhail lemdyanov <lacoustique@yandex.ru>
2024-12-17 20:33:51 +0500

    masks.py and utils.py checked by mypy, isort, flake8 and black

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,20 @@
 import logging
 
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("../Bank_App/logs/masks.log", 'w', encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("../Bank_App/logs/masks.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
+
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция, маскирующая номер карты"""
     logger.info("Выполняется проверка правильности номера карты")
     card_number_str = str(card_number)
     if len(card_number_str) != 16 or not card_number_str.isdigit():
-        logger.error('Неверный номер карты!')
-        raise ValueError('Неверный номер карты.')
+        logger.error("Неверный номер карты!")
+        raise ValueError("Неверный номер карты.")
     card_number_mask = f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
     logger.info("Номер карты замаскирован")
     return card_number_mask
@@ -24,8 +25,8 @@ def get_mask_account(account_number: str) -> str:
     logger.info("Выполняется проверка правильности номера карты")
     account_number_str = str(account_number)
     if len(account_number_str) != 20 or not account_number_str.isdigit():
-        logger.error('Неверный номер счета!')
-        raise ValueError('Неверный номер счета.')
+        logger.error("Неверный номер счета!")
+        raise ValueError("Неверный номер счета.")
     account_number_mask = f"**{account_number_str[-4:]}"
     logger.info("Номер счета замаскирован")
     return account_number_mask
